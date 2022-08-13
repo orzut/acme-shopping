afa491c6e69ae48056f1e366e26b34635270180d
Martin Murjas <martinmurjas@gmail.com>
2022-08-13 18:47:37 -0400

    removed unnecessary code and comments from server.js

--- server.js
+++ server.js
@@ -4,23 +4,8 @@ const { conn, User, Product } = require("./db");
 const setUp = async () => {
   try {
     await conn.sync({ force: true });
-    // await User.create({ username: "moe", password: "moe_pw" });
-    // const lucy = await User.create({ username: "lucy", password: "lucy_pw" });
-    const lucy = await User.create({
-      username: "lucy",
-      password: "lucy_pw",
-      firstName: "Lucy",
-      lastName: "Something",
-      email: "Lucy_Something@gmail.com",
-      phone: 2128932264,
-      shippingAddresStr: "test",
-      shippingAddresCity: "test",
-      shippingAddresState: "test",
-      shippingAddresZipcode: "test",
-      creditCardNumber: 1234123412341234,
-      creditCardExpirationDate: "2023-04-21",
-      creditCardPin: 1234,
-    });
+    await User.create({ username: "moe", password: "moe_pw" });
+    const lucy = await User.create({ username: "lucy", password: "lucy_pw" });
     const foo = await Product.create({ name: "foo" });
     const bar = await Product.create({ name: "bar" });
     await lucy.addToCart({ product: foo, quantity: 3 });
