ea3632ce86934ea5b80818f71453da482650a762
Bogdan Nikitin <bogdanonikitin@gmail.com>
2022-08-22 21:47:13 -0400

    seeding product data

---
+++ db/seedData/seedDataProducts.js
@@ -0,0 +1,46 @@
+const { Category, Genre, Product } = require('../')
+const { CATEGORIES } = require('./seedDataCategories')
+const { GENRES } = require('./seedDataGenres')
+const csv = require('csvtojson')
+
+const seedDataProducts = async () => {
+  try {
+    const products = await csv().fromFile('product_data.csv')
+    //console.log(products)
+
+    const categories = (
+      await Promise.all(
+        CATEGORIES.map(category => Category.create({ name: category }))
+      )
+    ).map(category => {
+      return { name: category.dataValues.name, id: category.dataValues.id }
+    })
+
+    const genres = (
+      await Promise.all(GENRES.map(genre => Genre.create({ name: genre })))
+    ).map(genre => {
+      return { name: genre.dataValues.name, id: genre.dataValues.id }
+    })
+
+    await Promise.all(
+      products.map(product => {
+        console.log(product)
+        return Product.create({
+          name: product.Name,
+          cost: product.Cost,
+          image: product.Img,
+          categoryId: categories.find(
+            category => product.Category === category.name
+          ).id,
+          genreId: genres.find(genre => product.Genre === genre.name).id
+        })
+      })
+    )
+  } catch (ex) {
+    console.log(ex)
+  }
+}
+
+// seedProductData()
+
+module.exports = { seedDataProducts }
--- db/seedData/syncAndSeed.js
+++ db/seedData/syncAndSeed.js
@@ -1,93 +1,108 @@
-const { conn, User, Product, CreditCard, Category, Genre } = require("../");
-const { USERS } = require("./seedDataUsers");
-const { CATEGORIES } = require("./seedDataCategories");
+const { conn, User, Product, CreditCard, Category, Genre } = require('../')
+const { USERS } = require('./seedDataUsers')
+const { CATEGORIES } = require('./seedDataCategories')
+const { GENRES } = require('./seedDataGenres')
 
-const { GENRES } = require("./seedDataGenres");
+const { seedDataProducts } = require('./seedDataProducts')
 
 const syncAndSeed = async () => {
   try {
-    await conn.sync({ force: true });
+    await conn.sync({ force: true })
+
+    //console.log(dataProducts)
+    seedDataProducts()
 
     await User.create({
-      firstName: "Lucy",
-      lastName: "Bar",
-      email: "lucy_bar@gmail.com",
-      password: "lucy_pw",
-      phone: "123-456-7890",
-      userType: "admin",
-    });
-    const createdUsers = await Promise.all(
-      USERS.map((user) => User.create(user))
-    );
+      firstName: 'Lucy',
+      lastName: 'Bar',
+      email: 'lucy_bar@gmail.com',
+      password: 'lucy_pw',
+      phone: '123-456-7890',
+      userType: 'admin'
+    })
+    const createdUsers = await Promise.all(USERS.map(user => User.create(user)))
     await CreditCard.create({
-      nameOnCard: "Moe Foo",
-      number: "1234123412341234",
+      nameOnCard: 'Moe Foo',
+      number: '1234123412341234',
       expirationMonth: 4,
       expirationYear: 25,
       pin: 573,
-      userId: createdUsers[3].id,
-    });
+      userId: createdUsers[3].id
+    })
 
-    const [teeShirts, hoodies, albums, hats, posters, accessories] =
-      await Promise.all(
-        CATEGORIES.map((category) => Category.create({ name: category }))
-      );
+    // const [
+    //   teeShirts,
+    //   hoodies,
+    //   albums,
+    //   hats,
+    //   posters,
+    //   accessories
+    // ] = await Promise.all(
+    //   CATEGORIES.map(category => Category.create({ name: category }))
+    // )
 
-    const [rock, pop, hipHop, country, electronic, metal, rAndB] =
-      await Promise.all(GENRES.map((genre) => Genre.create({ name: genre })));
+    // const [
+    //   rock,
+    //   pop,
+    //   hipHop,
+    //   country,
+    //   electronic,
+    //   metal,
+    //   rAndB
+    // ] = await Promise.all(GENRES.map(genre => Genre.create({ name: genre })))
 
-    //Temporary product seeding for front end development
-    await Product.create({
-      name: "Metallica T-Shirt",
-      cost: 5.99,
-      image:
-        "https://merchbar.imgix.net/product/105/6519/2389400944722/fzj7w7CRMetallica_-_Doris_-_Front.jpg?quality=60&auto=compress,format&w=390&h=390",
-      categoryId: teeShirts.id,
-      genreId: rock.id,
-    });
-    await Product.create({
-      name: "Smashing Pumpkins T-Shirt",
-      cost: 15.99,
-      image:
-        "https://merchbar.imgix.net/product/105/6519/4365059915858/KnXhz2RMSmash2.png?quality=60&auto=compress,format&w=390&h=390",
-      categoryId: teeShirts.id,
-      genreId: rock.id,
-    });
-    await Product.create({
-      name: "Jimi Hendrix T-shirt",
-      cost: 8.99,
-      image:
-        "https://merchbar.imgix.net/product/105/6519/4185688703058/JU8PGZhV55w_0a470b77-592d-4fb8-81b4-e848a4d00b95.png?quality=60&auto=compress,format&w=390&h=390",
-      categoryId: teeShirts.id,
-      genreId: rock.id,
-    });
-    await Product.create({
-      name: "N-Sync T-Shirt",
-      cost: 10.99,
-      image:
-        "https://merchbar.imgix.net/product/105/6519/4184831819858/J5vxYdDg70w_6f27a6e9-3226-4220-92b1-ec237a424f8b.png?quality=60&auto=compress,format&w=390&h=390",
-      categoryId: teeShirts.id,
-      genreId: pop.id,
-    });
-    await Product.create({
-      name: "Pink Floyd Album",
-      cost: 20.99,
-      image:
-        "https://merchbar.imgix.net/product/vinylized/upc/27/888751842717.jpg?quality=60&auto=compress,format&w=390&h=390",
-      categoryId: albums.id,
-      genreId: rock.id,
-    });
-    await Product.create({
-      name: "Kendrick Lamar Album",
-      cost: 20.99,
-      image:
-        "https://merchbar.imgix.net/product/4/1616/19226798/T830LL-1540265001-3601x3601-1540264977-007_Kendrick%20Lamar_Good%20Kid,%20M%20A%20A%20D%20City_01.jpg?quality=60&auto=compress,format&w=390&h=390",
-      categoryId: albums.id,
-      genreId: hipHop.id,
-    });
+    // //Temporary product seeding for front end development
+    // await Product.create({
+    //   name: 'Metallica T-Shirt',
+    //   cost: 5.99,
+    //   image:
+    //     'https://merchbar.imgix.net/product/105/6519/2389400944722/fzj7w7CRMetallica_-_Doris_-_Front.jpg?quality=60&auto=compress,format&w=390&h=390',
+    //   categoryId: teeShirts.id,
+    //   genreId: rock.id
+    // })
+    // await Product.create({
+    //   name: 'Smashing Pumpkins T-Shirt',
+    //   cost: 15.99,
+    //   image:
+    //     'https://merchbar.imgix.net/product/105/6519/4365059915858/KnXhz2RMSmash2.png?quality=60&auto=compress,format&w=390&h=390',
+    //   categoryId: teeShirts.id,
+    //   genreId: rock.id
+    // })
+    // await Product.create({
+    //   name: 'Jimi Hendrix T-shirt',
+    //   cost: 8.99,
+    //   image:
+    //     'https://merchbar.imgix.net/product/105/6519/4185688703058/JU8PGZhV55w_0a470b77-592d-4fb8-81b4-e848a4d00b95.png?quality=60&auto=compress,format&w=390&h=390',
+    //   categoryId: teeShirts.id,
+    //   genreId: rock.id
+    // })
+    // await Product.create({
+    //   name: 'N-Sync T-Shirt',
+    //   cost: 10.99,
+    //   image:
+    //     'https://merchbar.imgix.net/product/105/6519/4184831819858/J5vxYdDg70w_6f27a6e9-3226-4220-92b1-ec237a424f8b.png?quality=60&auto=compress,format&w=390&h=390',
+    //   categoryId: teeShirts.id,
+    //   genreId: pop.id
+    // })
+    // await Product.create({
+    //   name: 'Pink Floyd Album',
+    //   cost: 20.99,
+    //   image:
+    //     'https://merchbar.imgix.net/product/vinylized/upc/27/888751842717.jpg?quality=60&auto=compress,format&w=390&h=390',
+    //   categoryId: albums.id,
+    //   genreId: rock.id
+    // })
+    // await Product.create({
+    //   name: 'Kendrick Lamar Album',
+    //   cost: 20.99,
+    //   image:
+    //     'https://merchbar.imgix.net/product/4/1616/19226798/T830LL-1540265001-3601x3601-1540264977-007_Kendrick%20Lamar_Good%20Kid,%20M%20A%20A%20D%20City_01.jpg?quality=60&auto=compress,format&w=390&h=390',
+    //   categoryId: albums.id,
+    //   genreId: hipHop.id
+    // })
   } catch (ex) {
-    console.log(ex);
+    console.log(ex)
   }
-};
+}
 
-module.exports = { syncAndSeed };
+module.exports = { syncAndSeed }
